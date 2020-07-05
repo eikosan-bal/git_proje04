a6fdfb74e75796fb4fd3cd95a3e388396b2971ce
eikosan-bal <67152996+eikosan-bal@users.noreply.github.com>
2020-07-05 22:14:02 +0300

    Update ClassOne.java
    Bakalim oldu mu

--- src/git_proje04/ClassOne.java
+++ src/git_proje04/ClassOne.java
@@ -5,6 +5,7 @@ public class ClassOne {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ilk commit local maserdan");
+		System.out.println("Bakalim oldu mu");
 
 	}
 
